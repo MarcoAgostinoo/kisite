6af1fccf49c86127fd3df81545f0cd92754edb77
MarcoAgostinoMelo <marco.agostinoo@hotmail.com>
2025-01-14 01:22:20 -0300

    ajuste1214514012025

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['images.unsplash.com' ,  'flowbite.s3.amazonaws.com', 'tailwindui.com' , 'https://backend-nodemailer-git-main-marcos-projects-83aa3827.vercel.app','https://backend-nodemailer-git-main-marcos-projects-83aa3827.vercel.app/', 'https://backend-nodemailer-rho.vercel.app/', 'https://backend-nodemailer-rho.vercel.app'],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    domains: [
+      'images.unsplash.com', 
+      'flowbite.s3.amazonaws.com', 
+      'tailwindui.com', 
+      'backend-nodemailer-git-main-marcos-projects-83aa3827.vercel.app', 
+      'backend-nodemailer-rho.vercel.app'
+    ],
+  },
+};
+
+export default nextConfig;
