2f5105fe4b0938363e246bdb60b3edde5851cc46
MarcoAgostinoMelo <marco.agostinoo@hotmail.com>
2025-01-14 01:00:41 -0300

    ajustenextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ['images.unsplash.com' ,  'flowbite.s3.amazonaws.com', 'tailwindui.com' , 'https://backend-nodemailer-rho.vercel.app/captcha/captcha'],
+      domains: ['images.unsplash.com' ,  'flowbite.s3.amazonaws.com', 'tailwindui.com' , 'https://backend-nodemailer-rho.vercel.app/captcha/captcha', 'https://backend-nodemailer-rho.vercel.app/','https://backend-nodemailer-rho.vercel.app'],
     },
   };
   
