61f8721b89cff1740bb9f599779896c062d5e27d
MarcoAgostinoMelo <marco.agostinoo@hotmail.com>
2025-01-14 01:16:58 -0300

    daksdklasjdklasjdklasqueiweuo

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ['images.unsplash.com' ,  'flowbite.s3.amazonaws.com', 'tailwindui.com' , 'https://backend-nodemailer-git-main-marcos-projects-83aa3827.vercel.app','https://backend-nodemailer-git-main-marcos-projects-83aa3827.vercel.app/'],
+      domains: ['images.unsplash.com' ,  'flowbite.s3.amazonaws.com', 'tailwindui.com' , 'https://backend-nodemailer-git-main-marcos-projects-83aa3827.vercel.app','https://backend-nodemailer-git-main-marcos-projects-83aa3827.vercel.app/', 'https://backend-nodemailer-rho.vercel.app/', 'https://backend-nodemailer-rho.vercel.app'],
     },
   };
   
