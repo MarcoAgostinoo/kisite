6572df3ef96f7fa837717f51bcb806dc00787069
MarcoAgostinoMelo <marco.agostinoo@hotmail.com>
2025-02-14 02:55:19 -0300

    iuewiqoquwioadksdjasl

--- app/pages/article/[slug]/page.tsx
+++ app/pages/article/[slug]/page.tsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 import { Article } from "../../../types/Article";
+import NavBar from "@/app/components/navbar/NavBar";
+import CustomFooter from "@/app/components/footer/CustomFooter";
 
 interface PageProps {
   params: {
@@ -31,7 +33,7 @@ export default async function ArticlePage({ params }: PageProps) {
       headers: {
         Authorization: `Bearer ${process.env.STRAPI_API_TOKEN}`,
       },
-    }
+    },
   );
 
   const articles: Article[] = res.data.data;
@@ -43,47 +45,60 @@ export default async function ArticlePage({ params }: PageProps) {
   const article = articles[0];
 
   const coverUrl = article.cover?.url ? fixImageUrl(article.cover.url) : null;
-  const avatarUrl = article.author?.avatar?.url ? fixImageUrl(article.author.avatar.url) : null;
+  const avatarUrl = article.author?.avatar?.url
+    ? fixImageUrl(article.author.avatar.url)
+    : null;
 
   return (
-    <div className="container mx-auto p-6">
-      <h1 className="text-4xl font-bold mb-4">{article.title}</h1>
+    <div>
+      <NavBar />
+      <div className="container mx-auto mt-24 p-6 lg:mt-56">
+        <h1 className="mb-4 text-4xl font-bold">{article.title}</h1>
 
-      {coverUrl && (
-        <Image
-          src={coverUrl}
-          alt="Imagem do artigo"
-          width={400}
-          height={600}
-          priority
-          className="h-auto mb-4 rounded object-contain"
-        />
-      )}
+        {coverUrl && (
+          <Image
+            src={coverUrl}
+            alt="Imagem do artigo"
+            width={800} // Ajuste o valor conforme necessário
+            height={400} // Ajuste o valor conforme necessário
+            priority
+            style={{ width: "100%", height: "auto", maxHeight: "400px" }}
+            className="mb-4 rounded object-cover"
+          />
+        )}
 
-      <p className="mb-4">{article.description}</p>
+        <p className="mb-4">{article.description}</p>
 
-      {article.author && (
-        <div className="flex items-center mb-4">
-          {avatarUrl && (
-            <Image
-              src={avatarUrl}
-              alt={`Avatar de ${article.author.name}`}
-              width={50}
-              height={50}
-              className="rounded-full mr-2"
+        {article.author && (
+          <div className="mb-4 flex items-center">
+            {avatarUrl && (
+              <Image
+                src={avatarUrl}
+                alt={`Avatar de ${article.author.name}`}
+                width={50}
+                height={50}
+                className="mr-2 rounded-full"
+              />
+            )}
+            <p className="text-sm text-gray-600">Por: {article.author.name}</p>
+          </div>
+        )}
+
+        <p className="mb-4 text-xs text-gray-500">
+          Publicado em:{" "}
+          {new Date(article.publishedAt).toLocaleDateString("pt-BR")}
+        </p>
+
+        {article.blocks &&
+          article.blocks.map((block) => (
+            <div
+              key={block.id}
+              dangerouslySetInnerHTML={{ __html: block.body }}
+              className="mb-4"
             />
-          )}
-          <p className="text-sm text-gray-600">Por: {article.author.name}</p>
-        </div>
-      )}
-
-      <p className="text-xs text-gray-500 mb-4">
-        Publicado em: {new Date(article.publishedAt).toLocaleDateString("pt-BR")}
-      </p>
-
-      {article.blocks && article.blocks.map((block) => (
-        <div key={block.id} dangerouslySetInnerHTML={{ __html: block.body }} className="mb-4" />
-      ))}
+          ))}
+      </div>
+      <CustomFooter />
     </div>
   );
 }
